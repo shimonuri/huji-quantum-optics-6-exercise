ae9755978a4b4be6eac1dfb91137508451564c4d
Uri Shimon <54213090+shimonuri@users.noreply.github.com>
2023-05-23 11:31:06 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,10 +1,27 @@
+import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({"font.size": 22})
+
 
 def plot_experimental_data(filename):
-    data = pandas.read_csv("data/Exercise 6 Data.csv", names=["lamda", "coherence"])
-    plt.plot(data)
+    data = pandas.read_csv(filename, names=["lamda", "coherence"])
+    data.sort_values(by=["lamda"], inplace=True)
+    plt.plot(data["lamda"], data["coherence"], "o", label="Experimental Data")
+    plt.xlabel(r"$Nw$")
+    plt.ylabel("$g^{(2)}(0)$")
 
 
+def plot_theoretical_data():
+    theoretical_g = lambda x: (x * (x + 2)) / ((1 + x) ** 2)
+    x = np.linspace(0, 1.3, 100)
+    y = [theoretical_g(i) for i in x]
+    plt.plot(x, y, label="Theoretical")
 
+
+if __name__ == "__main__":
+    plot_experimental_data("data/Exercise 6 Data.csv")
+    plot_theoretical_data()
+    plt.legend()
+    plt.show()
